91fa7dc377ce8dcb5fdb22485ce8a4703445bad0
Doroteo111 <ladoritaesta@gmail.com>
2023-03-07 12:57:51 +0100

    1,2

--- Assets/SCRPITS/EXERCISE_1.cs
+++ Assets/SCRPITS/EXERCISE_1.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class EXERCISE_1 : MonoBehaviour
 {
     //Instantiate a given number of cubes in random positions with random scales.
+
+    private float randomZ = 6f;
+    private float randomX = 6f;
+    private float randomY= 1f;
+
+    public GameObject cubo;
+    
+
+    
     void Start()
     {
-        
-    }
+        for (int i = 0; i <= 10; i++)
+        {
+            randomX = Random.Range(6, -6);
+        randomZ = Random.Range(6, -6);
+        randomY = Random.Range(1, -1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 position = new Vector3(randomX, randomY, randomZ);
+
+        GameObject GO =Instantiate(cubo, position, Quaternion.identity);
+
+            GO.transform.localScale = Vector3.one * Random.Range(1,3);
+
+        }
+
+
+
     }
+   
+
+
+
+    
+    
+      
+    
+   
 }
--- Assets/SCRPITS/EXERCISE_2.cs
+++ Assets/SCRPITS/EXERCISE_2.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EXERCISE_2 : MonoBehaviour
 {
     //Make a timer with a corroutine. You must display the seconds on the screen with a Game Object TextMeshPro.
-    void Start()
+    public int counter = 0;
+    public TextMeshProUGUI text;
+
+    private void Start()
     {
-        
+
+        StartCoroutine(countDown());
     }
 
-    // Update is called once per frame
-    void Update()
+   private IEnumerator countDown()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            text.text = $"{counter}";
+            counter++;
+        }
+       
     }
+
 }
