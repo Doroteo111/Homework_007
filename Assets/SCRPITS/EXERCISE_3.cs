63ad645bb2c0ad5c6bb46b11bc49373c44e580af
Doroteo111 <ladoritaesta@gmail.com>
2023-03-08 10:45:11 +0100

    exercise3 done

--- Assets/SCRPITS/EXERCISE_3.cs
+++ Assets/SCRPITS/EXERCISE_3.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EXERCISE_3 : MonoBehaviour
 {
     //: Create a variable named value. Display the content of this variable in a Game Object TextMeshPro.
     //Each time you click the up arrow, you must add 1 to value and display the updated result.
     //Each time you click the down arrow, you must subtract 1 from value and display the updated result.
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    
+    public TextMeshProUGUI text;
+    public int value = 0;
+
     void Update()
     {
-        
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            text.text = $"{value}";
+           value++;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            text.text = $"{value}";
+           value--;
+        }
+
     }
+
+    
 }
--- Assets/SCRPITS/EXERCISE_4.cs
+++ Assets/SCRPITS/EXERCISE_4.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class EXERCISE_4 : MonoBehaviour
 {
     //With a coroutine and a for loop, make a Game Object change its position a finite number of times every 2 seconds.
-    void Start()
+
+    private float randomZ = 1f; //-6
+    private float randomX = 7f; //-7
+    private float randomY = 5f; //-3
+
+    public GameObject cubo;
+
+    private void Start()
     {
-        
+        randomX = Random.Range(7, -3);
+        randomZ = Random.Range(1, -6);
+        randomY = Random.Range(5, -3);
+
+        Vector3 position = new Vector3(randomX, randomY, randomZ);
+
+        GameObject GO = Instantiate(cubo, position, Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    /*for (int i = 0; i <= 10; i++)
+        {
+            randomX = Random.Range(6, -6);
+        randomZ = Random.Range(6, -6);
+        randomY = Random.Range(1, -1);
+
+        Vector3 position = new Vector3(randomX, randomY, randomZ);
+
+        GameObject GO =Instantiate(cubo, position, Quaternion.identity);
+
+            GO.transform.localScale = Vector3.one * Random.Range(1,3); */
+
+
 }
